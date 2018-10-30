0b06012284dcfc1d961fc4b20343613c19d5581b
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-30 15:25:45 -0500

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,45 @@
 // Code ControlledInput Component Here
+import React, { Component } from "react";
+
+export default class ControlledInput extends Component {
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  };
+
+  handleChange = event => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value
+      },
+      () => console.log("===>", event.target.name, event.target.value)
+    );
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input
+            type="text"
+            id="firstName"
+            onChange={event => this.handleChange(event)}
+            name="firstName"
+            value={this.state.firstName}
+          />
+          <input
+            type="text"
+            id="lastName"
+            onChange={event => this.handleChange(event)}
+            name="lastName"
+            value={this.state.lastName}
+          />
+        </form>
+      </div>
+    );
+  }
+}
